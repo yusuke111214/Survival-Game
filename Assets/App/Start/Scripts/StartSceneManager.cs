191eb3179883af7f6e29f39f66ed43bb315ba531
Yusuke Kanehira <y.kanehira@purpom-media-lab.com>
2025-03-03 08:29:14 +0900

    [Add]各シーンにAudio素材インポート、BGM設定、スタートシーンの効果音追加

--- Assets/App/Start/Scripts/StartSceneManager.cs
+++ Assets/App/Start/Scripts/StartSceneManager.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip clickSound;
+    private AudioSource audioSource; 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void OnStartButtonClicked()
     {
+        audioSource.PlayOneShot(clickSound);
         SceneManager.LoadScene("Scavenging");
     }
 }
